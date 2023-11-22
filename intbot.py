3463e2de57668216594bdbc0fb00678d71f28049
Bernotese <jannesgoebel00@gmail.com>
2023-11-22 23:04:04 +0100

    Added new content and fix join function

--- intbot.py
+++ intbot.py
@@ -1,7 +1,7 @@
 from decouple import config
 from interactions import slash_command, slash_option, SlashContext, OptionType
 from interactions import Client, Intents, listen
-from beanie import Document, Indexed, init_beanie
+from beanie import Document, init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import List
 from interactions import Embed, StringSelectMenu
@@ -9,39 +9,61 @@ from interactions import Embed, StringSelectMenu
 key = config('API_KEY')
 
 bot = Client(intents=Intents.DEFAULT)
-# intents are what events we want to receive from discord, `DEFAULT` is usually fine
 
-joined_players = []
+##################################
+##         Datenbanken          ##
+##################################
 
-# Database Stuff
+# "Players" ist die Datenbank für die Speicherung aller Spieler da. 
 class Players(Document):
-    discord_id: int
-    name: str
-    elo: int
-    wins: int
-    losses: int
+    discord_id: int # Discord UserID
+    name: str # Anzeigename - wählbar vom User
+    elo: int # Anzahl der Elopunkte [1-20]
+    wins: int # Anzahl der gewonnen Spiele
+    losses: int # Anzahl der verlorenen Spiele 
 
 class Games(Document):
-    members: List
-    team1: List[Players]
-    team2: List[Players]
-    winner: int
-    game_running: int # 1 game running / 0 game over
+    members: List[str] # Alle "joined" Players
+    team1: List[str] # Spieler in Team1
+    team2: List[str] # Spieler in Team2
+    winner: str # Wer hat gewonnen? team1/team2
+    game_running: bool # 1 game running / 0 game over
 
-mongo_client = AsyncIOMotorClient("mongodb://10.0.0.40:27017/")  # Ersetze die Verbindungs-URL entsprechend deiner MongoDB-Instanz
+##################################
+##         Funktionen           ##
+##################################
 
+async def check_player_exits(discord_id):
+    player = await Players.find_one({"discord_id": int(discord_id)})
+    s_player = str(player)
+    return s_player is not None
 
+async def get_player_by_discord_id(discord_id):
+    player = await Players.find_one({"discord_id": discord_id})
+    return player
 
-async def check_db_players(name):
-    existing_entry = await Players.find_one({"name": name})
-    return existing_entry is not None
-
-async def check_if_game_running():
-    running_game = await Games.find_one({"game_running": 1})
+async def check_games_status():
+    running_game = await Games.find_one({"game_running": True})
     print(running_game)
     return running_game is not None
 
-@slash_command(name="newplayer", description="Add new player | <Name> <Elo>")
+async def get_active_game():
+    game = await check_games_status()
+    game.id
+
+async def get_all_active_members_names():
+    active_game = await Games.find_one({"game_running": True})
+    members = active_game.members
+    return members
+
+##################################
+##         Commands             ##
+##################################
+
+@slash_command(
+        name="newplayer", 
+        description="Add new player | <Name> <Elo>"
+)
 @slash_option(
     name="name",
     description="Display Name",
@@ -54,77 +76,94 @@ async def check_if_game_running():
     required=True,
     opt_type=OptionType.INTEGER,
     min_value=1,
-    max_value=100
+    max_value=20
 )
 async def newpalyer(ctx: SlashContext, name: str, elo_points: int):
     # Erstellt einen neune Spieler (anlegen eines neuen Spielers)
-    # Userid
+
     discord_id = ctx.author.id
-    #DB Entry
-    player = Players(discord_id=int(discord_id), name=name, elo=elo_points, wins=0, losses=0)
-    
-    if await check_db_players(name):
+
+    if await check_player_exits(discord_id):
         await ctx.send(f"Hi {name}, du wurdest bereits angelegt.", ephemeral=True)
     else:
+        player = Players(discord_id=int(discord_id), name=name, elo=elo_points, wins=0, losses=0)
         await player.insert()
-        await ctx.send(f"Hi {name}, du wurdest mit einer Strat-Elo von {str(elo_points)} angelegt.", ephemeral=True)
+        await ctx.send(f"Hi {name}, du wurdest mit einer Start-Elo von {str(elo_points)} angelegt.", ephemeral=True)
 
-@slash_command(name="newgame", description="star new game")
+@slash_command(
+        name="newgame",
+        description="star new game"
+)
 async def newgame(ctx: SlashContext):
     # Startet eine neue Runde
-    if await check_if_game_running():
+    if await check_games_status():
         await ctx.send(f"Beende erst das letzte Spiel.", ephemeral=True)
         return
     else:
-        global new_game
-        new_game = Games(members=[], team1=[], team2=[], winner=0, game_running=1)
+        new_game = Games(members=[], team1=[], team2=[], winner="0", game_running=1)
         await new_game.insert()
         await ctx.send(f"Neues Spiel wurde erstellt.")
 
-@slash_command(name="join", description="Join a new game")
+@slash_command(
+        name="join",
+        description="Join a new game"
+)
 async def joingame(ctx: SlashContext):
     # Join einer bestehenden runde
-    user_id = ctx.author.id
-    if not check_if_game_running():
-        await ctx.send("Erstelle erst ein neues Spiel", ephemeral=True)
-        return
+    discord_id = str(ctx.author.id)
     
-    # Abruf des aktiven Spiels aus der Datenbank
-    active_game = await Games.find_one({"game_running": 1})
+    print(type(discord_id))
 
-    if active_game:
-        if user_id in active_game.members:
-            await ctx.send("Du bist bereits beigetreten.", ephemeral=True)
+    current_game = await Games.find_one({"game_running": True})
+    print(current_game)
+
+    player = await check_player_exits(discord_id)
+    print(player)
+
+    if not player:
+        await ctx.send("Lege dich erst an mit /newplayer", ephemeral=True)
+        return
+    else:
+        if not current_game:
+            await ctx.send("Erstelle erst ein neues Spiel", ephemeral=True)
+            return
+        if discord_id in current_game.members:
+            await ctx.send("Du bist bereits beigetreten", ephemeral=True)
+            return
         else:
-            active_game.members.append(user_id)
-            await active_game.replace(active_game)
-            await ctx.send("Du bist dem Spiel beigetreten.", ephemeral=True)
-
-async def get_all_active_members_names():
-    active_game = await Games.find_one({"game_running": 1})
-    members = active_game.members
-    return members
+            current_game.members.append(str(discord_id))
+            print (type(current_game.members))
+            print(current_game.members)
+            await current_game.save()
+            await ctx.send("Du bist dem Spiel beigetreten", ephemeral=True)
 
 @slash_command(name="startgame", description="start the game")
 async def startgame(ctx: SlashContext):
     game_members_ids = await get_all_active_members_names()
-    
     game_members = []
-    for player in game_members_ids:
-        p = await Players.find_one({"discord_id" : player})
-        name = p.name
-        print(f"Gefunden: " + name)
-        game_members.append(name)
-
-    print(len(game_members))
+    for discord_id in game_members_ids:
+        player = await get_player_by_discord_id(int(discord_id))
+        name = player.name
+        elo = player.elo
+        value = (name, elo)
+        game_members.append(value)
 
     game_info = Embed(title="Neues Spiel")
     game_info.set_thumbnail("https://upload.wikimedia.org/wikipedia/commons/1/1a/Faker_2020_interview.jpg")
-    
-    clear_names = " ,".join(game_members)
+    game_info.description = f"Es wurden {str(len(game_members))} Teilnehmer gefunden."
 
+    print(game_members)
 
-    game_info.description = f"{clear_names}"
+    clear_names = []
+    clear_elo = []
+    for p in game_members:
+        print(p)
+        print(p[0])
+        clear_names.append("".join(str(p[0]) + "\n"))
+        clear_elo.append(str(p[1]).join("\n"))
+        
+    game_info.add_field(name="Spieler:", value=str(clear_names))
+    game_info.add_field(name="Elo:", value=str(clear_elo))
     
     await ctx.send(embed=game_info)
 
@@ -153,6 +192,7 @@ async def test(ctx: SlashContext):
 @listen()  # this decorator tells snek that it needs to listen for the corresponding event, and run this coroutine
 async def on_ready():
     # This event is called when the bot is ready to respond to commands
+    mongo_client = AsyncIOMotorClient("mongodb://10.0.0.40:27017/")  # Ersetze die Verbindungs-URL entsprechend deiner MongoDB-Instanz
     await init_beanie(database=mongo_client.intbot, document_models=[Games])
     await init_beanie(database=mongo_client.intbot, document_models=[Players])
     print("Ready")
